ddc2b0d9b3b1cf52cf0519bff6798d2037aceb74
Ahnaf-cyber <ahnafcyberhero@gmail.com>
2026-03-12 19:22:28 +0600

    Delete Tutorial 10 directory

--- Tutorial 10/SortByDestination.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class SortByDestination implements Comparator<Shipment> {
-
-    @Override
-    public int compare(Shipment s1, Shipment s2) {
-        return s1.getDestination().compareToIgnoreCase(s2.getDestination());
-    }
-}
--- Tutorial 10/SortByWeight.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class SortByWeight implements Comparator<Shipment> {
-
-    @Override
-    public int compare(Shipment s1, Shipment s2) {
-        return Double.compare(s1.getWeight(), s2.getWeight());
-    }
-}
